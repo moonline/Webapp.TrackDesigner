ff6296cc726b906c0e9999a423b2097fc5dbb2bd
Tobias Blaser <tobiasblaser@colvarim.ch>
2014-08-21 12:19:21 +0200

    Add switch

--- Classes/Application/TrackController.ts
+++ Classes/Application/TrackController.ts
@@ -17,6 +17,7 @@ class TrackController {
 	
 		var railStraight: ShapeType = this.shapeTypes['railStraight'];
 		var railCurved: ShapeType = this.shapeTypes['railCurved'];
+		var switchStraight: ShapeType = this.shapeTypes['switchStraight'];
 		
 		var rail1:Shape = new Shape(railStraight,new Point(200,200),[]);
 		rail1.createConnectionPoint(railStraight.connectionPoints[1]);
@@ -31,6 +32,9 @@ class TrackController {
 		var rail6: Shape = Shape.createFromConnectionPoint(railCurved, rail5.getConnectionPoints()[1]);
 		var rail7: Shape = Shape.createFromConnectionPoint(railStraight, rail6.getConnectionPoints()[1]);
 		var rail8: Shape = Shape.createFromConnectionPoint(railStraight, rail7.getConnectionPoints()[1]);
+		var switch1: Shape = Shape.createFromConnectionPoint(switchStraight, rail8.getConnectionPoints()[1]);
+		var rail9: Shape = Shape.createFromConnectionPoint(railCurved, switch1.getConnectionPoints()[1]);
+		var rail10: Shape = Shape.createFromConnectionPoint(railStraight, switch1.getConnectionPoints()[2]);
 		
 		this.layout.addShape(rail2);
 		this.layout.addShape(rail3);
@@ -40,6 +44,9 @@ class TrackController {
 		this.layout.addShape(rail6);
 		this.layout.addShape(rail7);
 		this.layout.addShape(rail8);
+		this.layout.addShape(switch1);
+		this.layout.addShape(rail9);
+		this.layout.addShape(rail10);
 		
 		this.layout.draw();
 		
@@ -58,9 +65,16 @@ class TrackController {
 			railCurved: new ShapeType(
 				"Rail curved",
 				[new Vector(62.3424, 0.9906, 0.96875),new Vector(62.3424, 0.5094, 0.53125)],
-				136.9144,
-				69.9295,
+				143.13777, // 136.9144
+				100.91007, // 69.9295
 				"Resources/Img/Tracks/railCurved.png"		
+			),
+			switchStraight: new ShapeType(
+				"Switch straight",
+				[new Vector(131.52707, 0.03899, 0.0),new Vector(131.52707, 0.46101, 0.5),new Vector(131.52707, 0.96101, 0.0)],
+				255.2,
+				159.5,
+				"Resources/Img/Tracks/switchStraight.png"
 			)
 		};			
 	}
