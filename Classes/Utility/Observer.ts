317b1639474ce37701813067760e3d27ab93dd3e
Moonline <tobiasblaser@colvarim.ch>
2021-12-27 10:53:01 -0300

    Update page app to latest

--- Classes/Utility/Observer.ts
+++
@@ -1,8 +0,0 @@
-import EventType = require("Classes/Utility/EventType");
-import Observable = require("Classes/Utility/Observable");
-
-interface Observer {
-	notify(event: EventType, notifier: Observable, subject: any): void;
-}
-
-export = Observer;
